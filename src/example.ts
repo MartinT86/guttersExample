2d273a92d2911792f4807fa4fcf71214aa4e77da
Martin Tierney <martin.tierney86@hotmail.co.uk>
2021-01-27 22:42:25 +0000

    initial

---
+++ src/example.ts
@@ -0,0 +1,12 @@
+export const blah = (beep: string) => {
+    const asd = 123
+    if (beep === 'cat') {
+        return false
+    }
+    return true
+}
+
+export const boo = () => {
+    const sdf = 345
+    return sdf
+}
---
+++ test/example.test.ts
@@ -0,0 +1,7 @@
+import { blah } from '../src/example'
+
+describe('Name of the group', () => {
+    it('should ', () => {
+        expect(blah('asd')).toEqual(true)
+    });
+});
